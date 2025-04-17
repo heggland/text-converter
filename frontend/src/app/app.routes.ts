29a6ae92ba2bdd9f7565046b5422f11b84570303
Kjetil Heggland <kjetilheggland@gmail.com>
2025-04-17 14:12:42 +0200

    Update routing, added pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {ConvertComponent} from './convert/convert.component';
+import {DocsComponent} from './docs/docs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: ConvertComponent},
+  {path: 'docs', component: DocsComponent},
+];
